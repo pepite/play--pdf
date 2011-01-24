d97e2c329f7bb473f7d7fffde67e73a221a60d53
Johno Crawford <johno@hellface.com>
2011-01-24 22:17:21 +0100

    Fixed build script, updated yahp libraries (v1.2.20b), removed patches.

--- app/PDFPlugin.java
+++ app/PDFPlugin.java
@@ -1,15 +1,5 @@
-import play.Play;
 import play.PlayPlugin;
-import play.libs.IO;
-import play.libs.MimeTypes;
-import play.modules.pdf.PDF;
-import play.modules.pdf.RenderPDFTemplate;
-import play.mvc.Http;
 import play.mvc.Router;
-import play.mvc.results.Result;
-import play.vfs.VirtualFile;
-
-import java.io.File;
 
 public class PDFPlugin extends PlayPlugin {
 
