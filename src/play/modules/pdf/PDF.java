6d6516e349862f645bcba969b99e697790a649e0
Stephane Epardaud <stef@epardaud.fr>
2011-02-03 18:05:41 +0100

    Refactored a lot

--- src/play/modules/pdf/PDF.java
+++ src/play/modules/pdf/PDF.java
@@ -23,8 +23,15 @@
  */
 package play.modules.pdf;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
 import org.allcolor.yahp.converter.IHtmlToPdfTransformer;
 import org.apache.commons.io.FilenameUtils;
+
 import play.Play;
 import play.classloading.enhancers.LocalvariablesNamesEnhancer;
 import play.data.validation.Validation;
@@ -32,14 +39,11 @@ import play.exceptions.PlayException;
 import play.exceptions.TemplateNotFoundException;
 import play.exceptions.UnexpectedException;
 import play.mvc.Http;
+import play.mvc.Http.Request;
 import play.mvc.Scope;
 import play.templates.Template;
 import play.templates.TemplateLoader;
 import play.vfs.VirtualFile;
-
-import java.io.File;
-import java.util.List;
-import java.util.Map;
 
 public class PDF {
 
@@ -58,94 +62,49 @@ public class PDF {
         public IHtmlToPdfTransformer.PageSize pageSize = IHtmlToPdfTransformer.A4P;
     }
 
+    public static class MultiDocument {
+    	public String template;
+    	public Options options;
 
-    /**
-     * Render a specific template
-     *
-     * @param templateName The template name
-     * @param args         The template data
-     */
-    public static void renderTemplateAsPDF(String templateName, Options options, Object... args) {
-
-        // Template datas
-        Scope.RenderArgs templateBinding = Scope.RenderArgs.current();
-        for (Object o : args) {
-            List<String> names = LocalvariablesNamesEnhancer.LocalVariablesNamesTracer.getAllLocalVariableNames(o);
-            for (String name : names) {
-                templateBinding.put(name, o);
-            }
-        }
-        templateBinding.put("session", Scope.Session.current());
-        templateBinding.put("request", Http.Request.current());
-        templateBinding.put("flash", Scope.Flash.current());
-        templateBinding.put("params", Scope.Params.current());
-        try {
-            templateBinding.put("errors", Validation.errors());
-        } catch (Exception ex) {
-            throw new UnexpectedException(ex);
-        }
-        try {
-            Template template = TemplateLoader.load(templateName);
-            if (options == null) {
-                options = new Options();
-            }
-            if (options.filename == null) {
-                   options.filename = FilenameUtils.getBaseName(templateName) + ".pdf";
-            }
-            throw new RenderPDFTemplate(template, templateBinding.data, options);
-        } catch (TemplateNotFoundException ex) {
-            if (ex.isSourceAvailable()) {
-                throw ex;
-            }
-            StackTraceElement element = PlayException.getInterestingStrackTraceElement(ex);
-            if (element != null) {
-                throw new TemplateNotFoundException(templateName, Play.classes.getApplicationClass(element.getClassName()), element.getLineNumber());
-            } else {
-                throw ex;
-            }
-        }
+    	public MultiDocument(String template, Options options) {
+			this.template = template;
+			this.options = options;
+		}
     }
-
+    
     /**
      * Render the corresponding template
      *
      * @param args The template data
      */
     public static void renderPDF(Object... args) {
-        String templateName = null;
-        final Http.Request request = Http.Request.current();
-        final String format = request.format;
-
-        Options options = null;
-        if (args.length > 0 && args[0] instanceof Options) {
-            options = (Options) args[0];
-        } else if (args.length > 1 && args[1] instanceof Options) {
-            options = (Options) args[1];
-        }
-
-        if (args.length > 0 && args[0] instanceof String && LocalvariablesNamesEnhancer.LocalVariablesNamesTracer.getAllLocalVariableNames(args[0]).isEmpty()) {
-            templateName = args[0].toString();
-        } else {
-            templateName = request.action.replace(".", "/") + "." + (format == null ? "html" : format);
-        }
-        if (templateName.startsWith("@")) {
-            templateName = templateName.substring(1);
-            if (!templateName.contains(".")) {
-                templateName = request.controller + "." + templateName;
-            }
-            templateName = templateName.replace(".", "/") + "." + (format == null ? "html" : format);
-        }
-        VirtualFile file = Play.getVirtualFile(templateName);
-        if (file == null || !file.exists()) {
-            if (templateName.lastIndexOf("." + format) != -1) {
-                templateName = templateName.substring(0, templateName.lastIndexOf("." + format)) + ".html";
-            }
-            
-        }
-        renderTemplateAsPDF(templateName, options, args);
+    	// stuuuuuupid typing
+    	OutputStream os = null;
+    	writePDF(os, args);
     }
 
+    /**
+     * Render the corresponding template into a file
+     * @param file the file to render to, or null to render to the current Response object
+     * @param args the template data
+     */
     public static void writePDF(File file, Object... args) {
+    	try {
+    		OutputStream os = new FileOutputStream(file);
+    		writePDF(os);
+    		os.flush();
+			os.close();
+		} catch (IOException e) {
+			throw new UnexpectedException(e);
+		}
+    }
+    
+    /**
+     * Render the corresponding template into a file
+     * @param out the stream to render to, or null to render to the current Response object
+     * @param args the template data
+     */
+    public static void writePDF(OutputStream out, Object... args) {
         String templateName = null;
         final Http.Request request = Http.Request.current();
         final String format = request.format;
@@ -157,12 +116,17 @@ public class PDF {
             options = (Options) args[1];
         }
 
-
         if (args.length > 0 && args[0] instanceof String && LocalvariablesNamesEnhancer.LocalVariablesNamesTracer.getAllLocalVariableNames(args[0]).isEmpty()) {
             templateName = args[0].toString();
         } else {
             templateName = request.action.replace(".", "/") + "." + (format == null ? "html" : format);
         }
+        templateName = resolveTemplateName(templateName, request, format);
+        
+        renderTemplateAsPDF(out, templateName, options, args);
+    }
+
+    private static String resolveTemplateName(String templateName, Request request, String format) {
         if (templateName.startsWith("@")) {
             templateName = templateName.substring(1);
             if (!templateName.contains(".")) {
@@ -172,8 +136,20 @@ public class PDF {
         }
         VirtualFile template = Play.getVirtualFile(templateName);
         if (template == null || !template.exists()) {
-            templateName = templateName.substring(0, templateName.lastIndexOf("." + format)) + ".html";
+            if (templateName.lastIndexOf("." + format) != -1) {
+            	templateName = templateName.substring(0, templateName.lastIndexOf("." + format)) + ".html";
+            }
         }
+        return templateName;
+	}
+
+	/**
+     * Render a specific template
+     *
+     * @param templateName The template name
+     * @param args         The template data
+     */
+    public static void renderTemplateAsPDF(OutputStream out, String templateName, Options options, Object... args) {
         Scope.RenderArgs templateBinding = Scope.RenderArgs.current();
         for (Object o : args) {
             List<String> names = LocalvariablesNamesEnhancer.LocalVariablesNamesTracer.getAllLocalVariableNames(o);
@@ -190,20 +166,21 @@ public class PDF {
         } catch (Exception ex) {
             throw new UnexpectedException(ex);
         }
-        writeTemplateAsPDF(file, templateName, options, templateBinding.data);
-    }
-
-    /**
-     * Render a specific template
-     *
-     * @param templateName The template name
-     * @param args         The template data
-     */
-    public static void writeTemplateAsPDF(File file, String templateName, Options options, Map<String, Object> args) {
         try {
             Template template = TemplateLoader.load(templateName);
-            RenderPDFTemplate.writePDFAsFile(file, template, options, args);
-
+        	if (options == null) {
+        		options = new Options();
+        	}
+            if(out == null){
+            	// we're rendering to the current Response object
+            	if (options.filename == null) {
+            		options.filename = FilenameUtils.getBaseName(templateName) + ".pdf";
+            	}
+            	throw new RenderPDFTemplate(template, templateBinding.data, options);
+            }else{
+            	RenderPDFTemplate renderer = new RenderPDFTemplate(template, templateBinding.data, options);
+            	renderer.writePDF(out, Http.Request.current(), Http.Response.current());
+            }
         } catch (TemplateNotFoundException ex) {
             if (ex.isSourceAvailable()) {
                 throw ex;
--- src/play/modules/pdf/RenderPDFTemplate.java
+++ src/play/modules/pdf/RenderPDFTemplate.java
@@ -1,28 +1,25 @@
 package play.modules.pdf;
 
+import java.io.ByteArrayInputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.allcolor.yahp.converter.IHtmlToPdfTransformer;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
+
 import play.Logger;
 import play.Play;
-import play.exceptions.PlayException;
 import play.exceptions.TemplateNotFoundException;
 import play.exceptions.UnexpectedException;
-import play.modules.pdf.PDF.Options;
 import play.mvc.Http.Header;
 import play.mvc.Http.Request;
 import play.mvc.Http.Response;
 import play.mvc.results.Result;
 import play.templates.Template;
 import play.templates.TemplateLoader;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 200 OK
@@ -32,10 +29,11 @@ public class RenderPDFTemplate extends Result {
     public Template template;
     private String content;
     private PDF.Options options;
+    private List headerFooterList = new ArrayList();
 
     public RenderPDFTemplate(Template template, Map<String, Object> args, PDF.Options options) throws TemplateNotFoundException {
     	this(template, template.render(args), options);
-    	loadHeader(args);
+    	loadHeaderAndFooter(args);
     }
 
     public RenderPDFTemplate(Template template, String content, PDF.Options options) {
@@ -44,7 +42,7 @@ public class RenderPDFTemplate extends Result {
         this.options = options;
     }
 
-    private void loadHeader(Map<String, Object> args) throws TemplateNotFoundException {
+    private void loadHeaderAndFooter(Map<String, Object> args) throws TemplateNotFoundException {
     	if(!StringUtils.isEmpty(options.HEADER_TEMPLATE)){
     		Template template = TemplateLoader.load(options.HEADER_TEMPLATE);
     		HashMap<String, Object> args2 = new HashMap<String, Object>(args);
@@ -57,6 +55,16 @@ public class RenderPDFTemplate extends Result {
     		args2.remove("out");
     		options.FOOTER = template.render(args2);
     	}
+        if (!StringUtils.isEmpty(options.HEADER))
+            headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.HEADER, IHtmlToPdfTransformer.CHeaderFooter.HEADER));
+        if (!StringUtils.isEmpty(options.ALL_PAGES))
+            headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.ALL_PAGES, IHtmlToPdfTransformer.CHeaderFooter.ALL_PAGES));
+        if (!StringUtils.isEmpty(options.EVEN_PAGES))
+            headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.EVEN_PAGES, IHtmlToPdfTransformer.CHeaderFooter.EVEN_PAGES));
+        if (!StringUtils.isEmpty(options.FOOTER))
+            headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.FOOTER, IHtmlToPdfTransformer.CHeaderFooter.FOOTER));
+        if (!StringUtils.isEmpty(options.ODD_PAGES))
+            headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.ODD_PAGES, IHtmlToPdfTransformer.CHeaderFooter.ODD_PAGES));
 	}
 
 	protected static IHtmlToPdfTransformer transformer;
@@ -72,44 +80,19 @@ public class RenderPDFTemplate extends Result {
 
     public void apply(Request request, Response response) {
         try {
-            // TODO: Refactor, this is similar as writePDF
-            List headerFooterList = new ArrayList();
-            if (!StringUtils.isEmpty(options.HEADER))
-                headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.HEADER, IHtmlToPdfTransformer.CHeaderFooter.HEADER));
-            if (!StringUtils.isEmpty(options.ALL_PAGES))
-                headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.ALL_PAGES, IHtmlToPdfTransformer.CHeaderFooter.ALL_PAGES));
-            if (!StringUtils.isEmpty(options.EVEN_PAGES))
-                headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.EVEN_PAGES, IHtmlToPdfTransformer.CHeaderFooter.EVEN_PAGES));
-            if (!StringUtils.isEmpty(options.FOOTER))
-                headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.FOOTER, IHtmlToPdfTransformer.CHeaderFooter.FOOTER));
-            if (!StringUtils.isEmpty(options.ODD_PAGES))
-                headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.ODD_PAGES, IHtmlToPdfTransformer.CHeaderFooter.ODD_PAGES));
-
             response.setHeader("Content-Disposition", "inline; filename=\"" + options.filename + "\"");
             setContentTypeIfNotSet(response, "application/pdf");
             // FIX IE bug when using SSL
             if(request.secure && isIE(request))
             	response.setHeader("Cache-Control", "");
 
-            Map properties = Play.configuration;
-            String uri = request.getBase()+request.url;
-            // TODO: The page size should be configurable
-            try {
-                transformer.transform(new ByteArrayInputStream(content.getBytes("UTF-8")), uri, options.pageSize, headerFooterList,
-                        properties, response.out);
-            }
-            catch (final IHtmlToPdfTransformer.CConvertException e) {
-                throw e;
-            }
-            catch (final Exception e) {
-                throw new UnexpectedException(e);
-            } // end catch
+            renderPDF(response.out, request, response);
         } catch (Exception e) {
             throw new UnexpectedException(e);
         }
     }
 
-    private boolean isIE(Request request) {
+	private boolean isIE(Request request) {
 		if(!request.headers.containsKey("user-agent"))
 			return false;
 		
@@ -117,37 +100,17 @@ public class RenderPDFTemplate extends Result {
 		return userAgent.value().contains("MSIE");
 	}
 
-	public static void writePDFAsFile(File file, Template template, PDF.Options options, Map<String, Object> args) {
+	private void renderPDF(OutputStream out, Request request, Response response) throws Exception {
+        Map properties = Play.configuration;
+        String uri = request.getBase()+request.url;
+        transformer.transform(new ByteArrayInputStream(content.getBytes("UTF-8")), 
+        		uri, options.pageSize, headerFooterList,
+        		properties, out);
+	}
+	
+	public void writePDF(OutputStream out, Request request, Response response) {
         try {
-            List headerFooterList = new ArrayList();
-            IHtmlToPdfTransformer.PageSize pageSize = IHtmlToPdfTransformer.A4P;
-            if (options != null) {
-                if (!StringUtils.isEmpty(options.HEADER))
-                    headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.HEADER, IHtmlToPdfTransformer.CHeaderFooter.HEADER));
-                if (!StringUtils.isEmpty(options.ALL_PAGES))
-                    headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.ALL_PAGES, IHtmlToPdfTransformer.CHeaderFooter.ALL_PAGES));
-                if (!StringUtils.isEmpty(options.EVEN_PAGES))
-                    headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.EVEN_PAGES, IHtmlToPdfTransformer.CHeaderFooter.EVEN_PAGES));
-                if (!StringUtils.isEmpty(options.FOOTER))
-                    headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.FOOTER, IHtmlToPdfTransformer.CHeaderFooter.FOOTER));
-                if (!StringUtils.isEmpty(options.ODD_PAGES))
-                    headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter(options.ODD_PAGES, IHtmlToPdfTransformer.CHeaderFooter.ODD_PAGES));
-                pageSize = options.pageSize;
-            }
-
-            String content = template.render(args);
-            Map properties = Play.configuration;
-            String uri = Play.applicationPath.toURI().toURL().toExternalForm();
-            FileOutputStream out = new FileOutputStream(file);
-            try {
-                transformer.transform(new ByteArrayInputStream(content.getBytes("UTF-8")), uri, pageSize, headerFooterList,
-                        properties, out);
-                out.flush();
-                out.close();
-            }
-            catch (final IHtmlToPdfTransformer.CConvertException e) {
-                throw e;
-            }
+            renderPDF(out, request, response);
         } catch (Exception e) {
             throw new UnexpectedException(e);
         }
