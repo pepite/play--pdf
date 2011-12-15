5417bcde052dad97d6c415919528c542f47e7a0e
Stéphane Thomas <sthomas@lunatech.com>
2011-12-15 15:56:25 +0100

    Added a test case that simulate a call to an image using the same method than the PDF module itself.

--- samples-and-tests/pdf/test/BasicTest.java
+++ samples-and-tests/pdf/test/BasicTest.java
@@ -1,11 +1,46 @@
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import org.junit.Test;
+import play.Logger;
+import play.Play;
 import play.test.UnitTest;
 
 
 
 public class BasicTest extends UnitTest {
+  /**
+   * Tests the loading of an image.
+   * 
+   * @throws IOException in case of error
+   */
   @Test
-  public void aVeryImportantThingToTest() {
-    assertEquals(2, 1 + 1);
+  public void testLoadingOfImage() throws IOException {
+    String uri = getAbsoluteUrl("/images/help");
+    
+    Logger.debug("Testing loading of image at '%s'", uri);
+    
+    URL url = new URL(uri);
+    URLConnection connection = url.openConnection();
+    
+    // Decreases timeout as this local call should be quick to resolve (so we don't have to wait one minute before the 
+    // test fails)
+    connection.setReadTimeout(5000);
+    
+    assertTrue(connection.getContentLength() > 0);
+    assertSame("image/png", connection.getContentType());
+  }
+  
+  /**
+   * Builds the absolute url for the specified path.
+   * 
+   * @param path url path
+   * @return the absolute url for the specified path
+   */
+  private String getAbsoluteUrl(String path) {
+    String host = Play.configuration.getProperty("http.address", "127.0.0.1");
+    String port = Play.configuration.getProperty("http.port", "9000");
+    
+    return "http://" + host + ":" + port + path;
   }
 }
